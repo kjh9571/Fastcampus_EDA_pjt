0bd3574ef5d57ab43407bb4d68c1cb881ab352fd
kjh9571 <jhkim9571@gmail.com>
2022-05-11 13:04:48 +0900

    feat: Missing value handling

--- mini_pjt.py
+++ mini_pjt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
+# In[43]:
 
 
 import numpy as np
@@ -12,20 +12,30 @@ import openpyxl
 import os
 
 
-# In[3]:
+# In[52]:
 
 
 df = pd.read_excel("./파이널프로젝트_RAW_210329_210926.xlsx")
 
 
-# In[4]:
+# In[47]:
 
 
-df.head()
+df.info()
 
 
-# In[ ]:
+# In[53]:
 
 
+df['course_id'].replace('', np.nan, inplace=True)
+df['coupon_title'].fillna('사용안함', inplace=True)
+df['coupon_discount_amount'].fillna(0.0,inplace=True)
+df['sale_price'].replace('', np.nan, inplace=True)
+del df['tax_free_amount']
+df['pg'].fillna('ETC', inplace=True)
+df['subcategory_title'].fillna('해당없음', inplace=True)
 
+df.drop(index=47361, axis=0, inplace=True)
+df.dropna(subset=['course_id'], inplace=True)
+df.dropna(subset=['sale_price'],inplace=True)
 
