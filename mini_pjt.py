a63220a79e9a606c3374c36dc64f097f9b158382
kjh9571 <jhkim9571@gmail.com>
2022-05-11 11:32:52 +0900

    feat: Loading Raw data for EDA

--- mini_pjt.py
+++ mini_pjt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[2]:
 
 
 import numpy as np
@@ -10,6 +10,18 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import openpyxl
 import os
+
+
+# In[3]:
+
+
+df = pd.read_excel("./파이널프로젝트_RAW_210329_210926.xlsx")
+
+
+# In[4]:
+
+
+df.head()
 
 
 # In[ ]:
