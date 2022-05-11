d483893d29ccb8a91b701e9a9151fc62cce0ec8e
kjh9571 <jhkim9571@gmail.com>
2022-05-11 11:26:43 +0900

    feat: Import module

--- mini_pjt.py
+++ mini_pjt.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import openpyxl
+import os
+
+
 # In[ ]:
 
 
